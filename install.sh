492dfa201d7fede21c0f632de50c0f870bb47512
MadJoker <max26292@gmail.com>
2020-02-05 16:26:30 +0700

    optimize command

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt-get install -y php7.1 php7.1-common php7.1-mbstring php7.1-ldap php7.1-bcmath php7.1-dom php7.1-curl php7.1-gd php7.1-zip php-pear php7.1-mysql php-xdebug
-apt-get install -y php7.2 php7.2-common php7.2-mbstring php7.2-ldap php7.2-bcmath php7.2-dom php7.2-curl php7.2-gd php7.2-zip php7.2-mysql 
+apt-get install -y php7.1 php7.1-common php7.1-mbstring php7.1-ldap php7.1-bcmath php7.1-dom php7.1-curl php7.1-gd php7.1-zip php-pear php7.1-mysql php-xdebug \ 
+php7.2 php7.2-common php7.2-mbstring php7.2-ldap php7.2-bcmath php7.2-dom php7.2-curl php7.2-gd php7.2-zip php7.2-mysql 
 apt-get install -y mysql-server
 apt-get install -y curl perl 
 apt-get install npm && npm install -g npm && npm install -g yarn
