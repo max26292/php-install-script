8510c5c5d1be960002890bb3378401911c2fe2cc
Nguyen Nguyen Hoang <max26292@gmail.com>
2020-04-26 01:01:23 +0700

    add confirm for node

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ apt-get install -y php7.1 php7.1-common php7.1-mbstring php7.1-ldap php7.1-bcmat
 php7.2 php7.2-common php7.2-mbstring php7.2-ldap php7.2-bcmath php7.2-dom php7.2-curl php7.2-gd php7.2-zip php7.2-mysql 
 apt-get install -y mysql-server
 apt-get install -y curl perl 
-apt-get install npm && npm install -g npm && npm install -g yarn
+apt-get install npm -y && npm install -g npm && npm install -g yarn
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 nvm install v12.14.1
