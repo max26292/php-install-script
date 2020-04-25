d7b7c088b613b27e73ff8166b849403c3926e01b
Nguyen Nguyen Hoang <max26292@gmail.com>
2020-04-26 01:00:02 +0700

    add repo

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+apt-get install software-properties-common -y && add-apt-repository ppa:ondrej/php && apt-get update
 apt-get install -y php7.1 php7.1-common php7.1-mbstring php7.1-ldap php7.1-bcmath php7.1-dom php7.1-curl php7.1-gd php7.1-zip php-pear php7.1-mysql php-xdebug \ 
 php7.2 php7.2-common php7.2-mbstring php7.2-ldap php7.2-bcmath php7.2-dom php7.2-curl php7.2-gd php7.2-zip php7.2-mysql 
 apt-get install -y mysql-server
